cdd1131d80e0b22797b9a6196973b3accb4379d1
Paulina Sielicka <paulina.sielicka@rsqtechnologies.com>
2019-05-08 09:14:03 +0200

    Mean backlog size and mean backlog size change added to backlog metrics

--- evaluate_backlog_metrics.R
+++ evaluate_backlog_metrics.R
@@ -1,18 +1,29 @@
-evaluate_autocorrelation <- function (products) {
+evaluate_backlog_metrics <- function (products) {
     product_names <- c()
     autocorrelation_measures <- c()
+    backlog_mean_size <- c()
+    mean_backlog_size_change <- c()
     for(i in 1:nrow(products)){
        product = products[i,]
-       product_autocorrelation_measure = check_backlog_autocorrelation(product)
+       defect_backlog = read.csv(file=paste("data/", product[1], "/", product[2], "/", product[1], "_", product[2], "_backlog.csv", sep=""), header=TRUE, sep=",")
+       backlog_mean_size = c(backlog_mean_size, round(mean(defect_backlog$backlog_all), 2))      
+       
+       product_autocorrelation_measure = get_backlog_autocorrelation(product)
        autocorrelation_measures = c(autocorrelation_measures, product_autocorrelation_measure)
+        
+       backlog_size_change = round(get_mean_backlog_size_change(defect_backlog), 2)
+       mean_backlog_size_change = c(mean_backlog_size_change, backlog_size_change )
+        
        product_names = c(product_names, paste(product[1], product[2], sep = "_"))
     }
     result = data.frame(Products = product_names,
-                        Percent_of_autocorrelation_that_exceeds_sign_bounds = autocorrelation_measures)
-    write.table(result, file = "data/autocorrelation_evaluation.csv", sep=",")
+                        Percent_of_autocorrelation_that_exceeds_sign_bounds = autocorrelation_measures,
+                        Mean_backlog_size = backlog_mean_size,
+                        Mean_backlog_size_change = mean_backlog_size_change)
+    write.table(result, file = "data/backlogs_metrics.csv", sep=",")
 }    
 
-check_backlog_autocorrelation <- function(product){
+get_backlog_autocorrelation <- function(product){
     product_path = paste("data/", product[1], "/", product[2], "/", sep="")
     defect_backlog = read.csv(file=paste(product_path, product[1], "_", product[2], "_backlog.csv", sep=""), header=TRUE, sep=",")
     component_evaluation = read.csv(file=paste(product_path, "/predictions_evaluation.csv", sep=""), header=TRUE, sep=",")
@@ -32,7 +43,6 @@ check_backlog_autocorrelation <- function(product){
         backlog_all = diff(backlog_all)
         number_of_diff = number_of_diff-1
     } 
-    
     ts_length = length(backlog_all)
     autocorelation = acf(backlog_all, lag.max=ts_length, plot = FALSE)
     acf = autocorelation$acf
@@ -42,4 +52,14 @@ check_backlog_autocorrelation <- function(product){
         if (abs(acf[i]) > bound) number_of_correlated_lags = number_of_correlated_lags +1
     } 
     return(round(number_of_correlated_lags/ts_length * 100, 2))
+}
+
+get_mean_backlog_size_change <- function(defect_backlog){
+    backlog_size_diff = c()
+    for (i in 2:nrow(backlog)){
+        previous_week = defect_backlog[i-1,"backlog_all"]
+        current_week = defect_backlog[i,"backlog_all"]
+        backlog_size_diff = c(backlog_size_diff, abs(previous_week-current_week) )
+    }    
+    return(mean(backlog_size_diff, na.rm=TRUE))
 }
